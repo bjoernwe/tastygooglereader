5f7ba5272b4b8a43115e19f1b2f31a8eccff7378
Björn <bjoern.weghenkel@gmail.com>
2009-08-17 19:27:56 +0000

    license

--- trunk/content/TastyGoogleReader.js
+++ trunk/content/TastyGoogleReader.js
@@ -1,11 +1,12 @@
-/*
+/**
  * TastyGoogleReader
  *
  * Copyright (C) 2009 Björn Weghenkel
  *
  * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License version 3 as published by the
- * Free Software Foundation.
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
